8af175dda3e3e15e13542dc9cb4c76f7da5480be
Craig <craig-colliegitlab@cobradah.org>
2017-08-07 19:29:15 +1000

    Fixed the Non-Starters shortname

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -6,7 +6,7 @@ class Datum < ApplicationRecord
       {shortname: 'IN', longname: 'IN'},
       {shortname: 'OUT', longname: 'OUT'},
       {shortname: 'WD', longname: 'Withdrawn'},
-      {shortname: 'NS', longname: 'Non-Starter'}
+      {shortname: 'DNS', longname: 'Non-Starter'}
     ]
   end
 end
