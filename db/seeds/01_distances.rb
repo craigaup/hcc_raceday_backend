2ec8a111eaee56570e8bccbbad1a5d08a85dd58c
Craig <craig-colliegitlab@cobradah.org>
2018-09-22 21:25:16 +1000

    Allow distances for each year

--- db/seeds/01_distances.rb
+++ db/seeds/01_distances.rb
@@ -18,12 +18,14 @@ duesoonFrom = {'A': 'ST', 'B': 'A', 'C': 'B', 'D': 'C', 'E': 'D', 'F': 'E',
                'S': 'Q', 'T': 'Q', 'FIN': 'Q'}
 
 distances.each do |checkpoint, distance|
-  if Distance.find_by(checkpoint: checkpoint) == nil then
+  if Distance.find_by(checkpoint: checkpoint,
+                      year: DateTime.now.in_time_zone.year).nil? then
     longname = checkpointNames[checkpoint]
     print "INFO: Creating checkpoint #{checkpoint} at distance #{distance}\n"
     duesoon = ''
     duesoon = duesoonFrom[checkpoint] if duesoonFrom.key?(checkpoint)
-    Distance.create(checkpoint: checkpoint, year: DateTime.now.year,
+    Distance.create(checkpoint: checkpoint,
+                    year: DateTime.now.in_time_zone.year,
                     distance: distance, longname: longname,
                     duesoonfrom: duesoon)
   end
