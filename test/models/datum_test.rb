29a4f8f3e98fbe2cf03424f1da6081cde36680d6
Craig <craig-colliegitlab@cobradah.org>
2018-10-01 18:34:34 +1000

    Added tests for datum

--- test/models/datum_test.rb
+++ test/models/datum_test.rb
@@ -1,7 +1,62 @@
 require 'test_helper'
 
 class DatumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  def setup
+    %i[one two ].each do |sym|
+      datum = data(sym)
+      datum.year = DateTime.now.in_time_zone.year
+      datum.save
+    end
+    
+    @datum = data(:one)
+  end
+
+  test 'should be valid' do
+    pp @datum.errors unless @datum.valid?
+#byebug
+    assert @datum.valid?
+
+    %i[one two].each do |sym|
+      assert data(sym).valid?
+    end
+  end
+
+
+  test 'self.statusList' do
+    output = [
+      {shortname: 'IN', longname: 'IN'},
+      {shortname: 'OUT', longname: 'OUT'},
+      {shortname: 'WD', longname: 'Withdrawn'},
+      {shortname: 'DNS', longname: 'Non-Starter'}
+    ]
+
+    assert_equal output, Datum.statusList
+  end
+
+  test 'self.returnValue' do
+    assert_nil Datum.returnValue('MyString', 2017)
+    assert_equal @datum.data, Datum.returnValue(@datum.key,
+                                                DateTime.now.in_time_zone.year)
+    assert_equal @datum.data, Datum.returnValue(@datum.key)
+  end
+
+  test 'self.setValue' do
+    assert_not Datum.setValue(nil, 'a')
+
+    assert Datum.setValue('a', 'b')
+    assert_equal 'b', Datum.returnValue('a')
+
+    assert Datum.setValue('a', 'c', DateTime.now.in_time_zone.year)
+    assert_equal 'c', Datum.returnValue('a', DateTime.now.in_time_zone.year)
+
+    assert Datum.setValue('a', 'd', 2017)
+    assert_equal 'c', Datum.returnValue('a')
+    assert_equal 'd', Datum.returnValue('a', 2017)
+  end
+
+  # test 'self.' do
+  # test 'self.' do
+  #   Craft.send(:public, :getTimeFormat)
+  # byebug
   # end
 end
